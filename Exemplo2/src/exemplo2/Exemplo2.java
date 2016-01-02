5d721d18f85ca08ea9b0829f055f4f4a468c432d
ninafontaiña <ninafontaia@nina.home>
2016-01-02 10:55:13 +0100

    engadin unha visualizacion

--- Exemplo2/src/exemplo2/Exemplo2.java
+++ Exemplo2/src/exemplo2/Exemplo2.java
@@ -16,6 +16,7 @@ public class Exemplo2 {
      */
     public static void main(String[] args) {
         System.out.println(" exemplo2");
+        System.out.println(" unha modificacion no programa");
     }
     
 }
